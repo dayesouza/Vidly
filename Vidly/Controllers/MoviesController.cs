35929f6538485a7291b5af57ce7727005a75997c
Day Souza <day.carol18@gmail.com>
2019-03-20 00:43:02 -0300

    Added movies index

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -15,5 +15,26 @@ namespace Vidly.Controllers
             var movie = new Movie() { Name = "Shrek" };
             return View(movie);
         }
+
+    public ActionResult Edit(int id)
+    {
+      return Content("id:" + id);
+    }
+
+    public ActionResult Index(int? pageIndex, string sortBy)
+    {
+      if (!pageIndex.HasValue)
+      {
+        pageIndex = 1;
+      }
+
+      if (String.IsNullOrEmpty(sortBy))
+      {
+        sortBy="name";
+      }
+
+      return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+    }
+
     }
 }
